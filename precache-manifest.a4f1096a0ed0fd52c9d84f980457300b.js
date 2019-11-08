40e538701e2a03da9fd4ba1a08266492506bf242
naphtha <naphthanewman@gmail.com>
2019-11-08 00:55:57 +0000

    Updates

---
+++ precache-manifest.a4f1096a0ed0fd52c9d84f980457300b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5570fd5200be3cdd96b84f24d716fbee",
+    "url": "/dummy-text-generator/index.html"
+  },
+  {
+    "revision": "7524dbe4b820c3bb8974",
+    "url": "/dummy-text-generator/static/css/main.49760eee.chunk.css"
+  },
+  {
+    "revision": "f7abc6db6a293199c575",
+    "url": "/dummy-text-generator/static/js/2.bac227ea.chunk.js"
+  },
+  {
+    "revision": "7524dbe4b820c3bb8974",
+    "url": "/dummy-text-generator/static/js/main.ffdfac33.chunk.js"
+  },
+  {
+    "revision": "cf779c2bd29a69c4bd1b",
+    "url": "/dummy-text-generator/static/js/runtime-main.93451447.js"
+  }
+]);
